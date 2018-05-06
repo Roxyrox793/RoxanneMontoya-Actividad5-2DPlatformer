297bc8a926504a37798146c526e2a7c52a0c1211
Roxanne <roxyrox793@gmail.com>
2018-05-05 21:09:46 -0500

    Mario v2

---
+++ RoxanneMontoya-2DPlatformer /Assets/Scrips/charactercontrol.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class charactercontrol : MonoBehaviour {
+	float speed = 0.25f;
+	int coins = 0;
+
+	public Text contadorCoins;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.gameObject.transform.Translate (speed, 0f, 0f);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.gameObject.transform.Translate (-speed, 0f, 0f);
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * 3, ForceMode2D.Impulse);
+
+
+		}
+	}
+}
